981b97ca1c8b6202310574002ffcd3e419e75c8c
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-09-01 02:15:37 +0900

    use webfont

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,16 @@
 <script lang="ts">
-	export let name: string;
 </script>
 
+<svelte:head>
+	<link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@700&display=swap" rel="stylesheet">
+</svelte:head>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		font-family: "M PLUS Rounded 1c",sans-serif;
+		transform: rotate(0.03deg);
 	}
 </style>
