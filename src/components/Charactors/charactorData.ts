6e79f6905b5dce3b961e0c97ade1a7dfb90b8d15
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-09-04 04:32:43 +0900

    add sample Charactor

--- src/components/Charactors/charactorData.ts
+++ src/components/Charactors/charactorData.ts
@@ -1,21 +1,34 @@
 export interface CharactorData {
   account: string
-  name: string
+  name: TextIsColored[]
   src: string
-  selif: string
-  voice: string[]
+  selif: TextWithColor
+  voices: string[]
   cv: string
   detail: string
   url: string
 }
 
+interface TextWithColor {
+  text: string
+  color: [number, number, number, number]
+}
+
+interface TextIsColored {
+  text: string
+  colored: boolean
+}
+
 export const charactorDatas: CharactorData[] = [
   {
     account: 'https://trap.jp/author/ryoha/',
-    name: 'ryoha',
-    src: '/assets/sui_gray.png',
-    selif: '兄さんはいっつもそうです、仕方ない人ですね。',
-    voice: [],
+    name: 'ryoha'.split('').map(v => ({ text: v, colored: v === 'o'})),
+    src: '/assets/sui_right_back_white.png',
+    selif: {
+      text: '兄さんはいっつもそうです、仕方ない人ですね。',
+      color: [84, 84, 84, 1.0]
+    },
+    voices: ['/assets/桜のあと(all quartets lead to the？).mp3'],
     cv: 'Text-to-Speech',
     detail: '兄思いの妹',
     url: 'https://www.makura-soft.com/sakuranouta/index.html'
