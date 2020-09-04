1e7f30f5759c9a28c4332ca63fc97de647f89c96
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-09-05 08:32:12 +0900

    add sample work

---
+++ src/components/Gallery/Works.ts
@@ -0,0 +1,239 @@
+export interface Work {
+  title: string
+  subtitle: string
+  description: string[]
+  imgURLs: string[]
+  refLinks: RefLink[]
+}
+
+export interface RefLink {
+  title: string
+  url: string
+}
+
+export const works: Work[] = [
+  {
+    title: 'エロゲランチャー',
+    subtitle: 'エロゲ界のSteamを目指して',
+    description: [
+      '自動でインストールしたエロゲを探索して、自分で設定することなく起動やプレイ時間の管理ができるエロゲランチャーです。',
+      '個人プロジェクトです。',
+      'デスクトップアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
+      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています',
+    ],
+    imgURLs: [
+      '/assets/es-client-header.png',
+      '/assets/es-client-home-activity.png'
+    ],
+    refLinks: [
+      {
+        title: 'ユーザードキュメント',
+        url: 'https://ryoha000.hatenablog.com/entry/2020/08/02/215327'
+      },
+      {
+        title: 'Tweet',
+        url: 'https://twitter.com/ryoha_0216/status/1269247713213706242?s=20'
+      },
+      {
+        title: 'クライアント(ソースコード)',
+        url: 'https://github.com/ryoha000/es-client'
+      },
+      {
+        title: 'サーバー(ソースコード)',
+        url: 'https://github.com/ryoha000/esc_server'
+      },
+    ]
+  },
+  {
+    title: 'エロゲランチャー',
+    subtitle: 'エロゲ界のSteamを目指して',
+    description: [
+      '自動でインストールしたエロゲを探索して、自分で設定することなく起動やプレイ時間の管理ができるエロゲランチャーです。',
+      '個人プロジェクトです。',
+      'デスクトップアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
+      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています',
+    ],
+    imgURLs: [
+      '/assets/es-client-header.png',
+      '/assets/es-client-home-activity.png'
+    ],
+    refLinks: [
+      {
+        title: 'ユーザードキュメント',
+        url: 'https://ryoha000.hatenablog.com/entry/2020/08/02/215327'
+      },
+      {
+        title: 'Tweet',
+        url: 'https://twitter.com/ryoha_0216/status/1269247713213706242?s=20'
+      },
+      {
+        title: 'クライアント(ソースコード)',
+        url: 'https://github.com/ryoha000/es-client'
+      },
+      {
+        title: 'サーバー(ソースコード)',
+        url: 'https://github.com/ryoha000/esc_server'
+      },
+    ]
+  },
+  {
+    title: 'エロゲランチャー',
+    subtitle: 'エロゲ界のSteamを目指して',
+    description: [
+      '自動でインストールしたエロゲを探索して、自分で設定することなく起動やプレイ時間の管理ができるエロゲランチャーです。',
+      '個人プロジェクトです。',
+      'デスクトップアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
+      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています',
+    ],
+    imgURLs: [
+      '/assets/es-client-header.png',
+      '/assets/es-client-home-activity.png'
+    ],
+    refLinks: [
+      {
+        title: 'ユーザードキュメント',
+        url: 'https://ryoha000.hatenablog.com/entry/2020/08/02/215327'
+      },
+      {
+        title: 'Tweet',
+        url: 'https://twitter.com/ryoha_0216/status/1269247713213706242?s=20'
+      },
+      {
+        title: 'クライアント(ソースコード)',
+        url: 'https://github.com/ryoha000/es-client'
+      },
+      {
+        title: 'サーバー(ソースコード)',
+        url: 'https://github.com/ryoha000/esc_server'
+      },
+    ]
+  },
+  {
+    title: 'エロゲランチャー',
+    subtitle: 'エロゲ界のSteamを目指して',
+    description: [
+      '自動でインストールしたエロゲを探索して、自分で設定することなく起動やプレイ時間の管理ができるエロゲランチャーです。',
+      '個人プロジェクトです。',
+      'デスクトップアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
+      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています',
+    ],
+    imgURLs: [
+      '/assets/es-client-header.png',
+      '/assets/es-client-home-activity.png'
+    ],
+    refLinks: [
+      {
+        title: 'ユーザードキュメント',
+        url: 'https://ryoha000.hatenablog.com/entry/2020/08/02/215327'
+      },
+      {
+        title: 'Tweet',
+        url: 'https://twitter.com/ryoha_0216/status/1269247713213706242?s=20'
+      },
+      {
+        title: 'クライアント(ソースコード)',
+        url: 'https://github.com/ryoha000/es-client'
+      },
+      {
+        title: 'サーバー(ソースコード)',
+        url: 'https://github.com/ryoha000/esc_server'
+      },
+    ]
+  },
+  {
+    title: 'エロゲランチャー',
+    subtitle: 'エロゲ界のSteamを目指して',
+    description: [
+      '自動でインストールしたエロゲを探索して、自分で設定することなく起動やプレイ時間の管理ができるエロゲランチャーです。',
+      '個人プロジェクトです。',
+      'デスクトップアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
+      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています',
+    ],
+    imgURLs: [
+      '/assets/es-client-header.png',
+      '/assets/es-client-home-activity.png'
+    ],
+    refLinks: [
+      {
+        title: 'ユーザードキュメント',
+        url: 'https://ryoha000.hatenablog.com/entry/2020/08/02/215327'
+      },
+      {
+        title: 'Tweet',
+        url: 'https://twitter.com/ryoha_0216/status/1269247713213706242?s=20'
+      },
+      {
+        title: 'クライアント(ソースコード)',
+        url: 'https://github.com/ryoha000/es-client'
+      },
+      {
+        title: 'サーバー(ソースコード)',
+        url: 'https://github.com/ryoha000/esc_server'
+      },
+    ]
+  },
+  {
+    title: 'エロゲランチャー',
+    subtitle: 'エロゲ界のSteamを目指して',
+    description: [
+      '自動でインストールしたエロゲを探索して、自分で設定することなく起動やプレイ時間の管理ができるエロゲランチャーです。',
+      '個人プロジェクトです。',
+      'デスクトップアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
+      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています',
+    ],
+    imgURLs: [
+      '/assets/es-client-header.png',
+      '/assets/es-client-home-activity.png'
+    ],
+    refLinks: [
+      {
+        title: 'ユーザードキュメント',
+        url: 'https://ryoha000.hatenablog.com/entry/2020/08/02/215327'
+      },
+      {
+        title: 'Tweet',
+        url: 'https://twitter.com/ryoha_0216/status/1269247713213706242?s=20'
+      },
+      {
+        title: 'クライアント(ソースコード)',
+        url: 'https://github.com/ryoha000/es-client'
+      },
+      {
+        title: 'サーバー(ソースコード)',
+        url: 'https://github.com/ryoha000/esc_server'
+      },
+    ]
+  },
+  {
+    title: 'エロゲランチャー',
+    subtitle: 'エロゲ界のSteamを目指して',
+    description: [
+      '自動でインストールしたエロゲを探索して、自分で設定することなく起動やプレイ時間の管理ができるエロゲランチャーです。',
+      '個人プロジェクトです。',
+      'デスクトップアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
+      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています',
+    ],
+    imgURLs: [
+      '/assets/es-client-header.png',
+      '/assets/es-client-home-activity.png'
+    ],
+    refLinks: [
+      {
+        title: 'ユーザードキュメント',
+        url: 'https://ryoha000.hatenablog.com/entry/2020/08/02/215327'
+      },
+      {
+        title: 'Tweet',
+        url: 'https://twitter.com/ryoha_0216/status/1269247713213706242?s=20'
+      },
+      {
+        title: 'クライアント(ソースコード)',
+        url: 'https://github.com/ryoha000/es-client'
+      },
+      {
+        title: 'サーバー(ソースコード)',
+        url: 'https://github.com/ryoha000/esc_server'
+      },
+    ]
+  }
+]
