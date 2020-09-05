0fca333694e591d290c87766582912c3da31068e
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-09-06 07:46:20 +0900

    add Works data

--- src/components/Gallery/Works.ts
+++ src/components/Gallery/Works.ts
@@ -13,35 +13,23 @@ export interface RefLink {
 
 export const works: Work[] = [
   {
-    title: 'エロゲランチャー',
-    subtitle: 'エロゲ界のSteamを目指して',
+    title: 'りょはインフィニティ',
+    subtitle: '架空ADVゲーム公式サイト',
     description: [
-      '自動でインストールしたエロゲを探索して、自分で設定することなく起動やプレイ時間の管理ができるエロゲランチャーです。',
+      '架空ADVゲーム「りょはインフィニティ」の公式サイト風ポートフォリオです。',
       '個人プロジェクトです。',
-      'デスクトップアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
-      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています',
+      'クライアントは svelte + rollup + TypeScript で作成しています。',
+      'Azure Static Web App で自動デプロイされています。',
     ],
     imgURLs: [
-      '/assets/es-client-header.png',
-      '/assets/es-client-home-activity.png'
+      '/assets/portfolio01.png',
+      '/assets/portfolio02.png'
     ],
     refLinks: [
       {
-        title: 'ユーザードキュメント',
-        url: 'https://ryoha000.hatenablog.com/entry/2020/08/02/215327'
-      },
-      {
-        title: 'Tweet',
-        url: 'https://twitter.com/ryoha_0216/status/1269247713213706242?s=20'
-      },
-      {
-        title: 'クライアント(ソースコード)',
-        url: 'https://github.com/ryoha000/es-client'
-      },
-      {
-        title: 'サーバー(ソースコード)',
-        url: 'https://github.com/ryoha000/esc_server'
-      },
+        title: 'ソースコード',
+        url: 'https://github.com/ryoha000/portfolio'
+      }
     ]
   },
   {
@@ -50,8 +38,8 @@ export const works: Work[] = [
     description: [
       '自動でインストールしたエロゲを探索して、自分で設定することなく起動やプレイ時間の管理ができるエロゲランチャーです。',
       '個人プロジェクトです。',
-      'デスクトップアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
-      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています',
+      'デスクトップWebアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
+      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています。',
     ],
     imgURLs: [
       '/assets/es-client-header.png',
@@ -73,166 +61,85 @@ export const works: Work[] = [
       {
         title: 'サーバー(ソースコード)',
         url: 'https://github.com/ryoha000/esc_server'
+      }
+    ]
+  },
+  {
+    title: 'ポケモン対戦補助ツール',
+    subtitle: '第8世代の暇ツール',
+    description: [
+      'ポケモン剣盾発売一週間後に公開した対戦補助ツールです。',
+      'ダメージ計算や所持ポケモン、対戦成績の管理を容易にします。',
+      '個人プロジェクトです。',
+      '発売直後の公開だったためGoogle検索上位にきてユーザー数一万人を超えました。',
+      '現状(2020/09/06)メンテナンスやサーバーの都合上運用していません。',
+      'クライアントは React + TypeScript で作成しています。',
+      'サーバーサイドは GCP(Cloud Functions + Cloud SQL) + Python (+ Flask + SQLAlchemy) を使用しています',
+    ],
+    imgURLs: [
+      '/assets/tool4pokemon8th01.png',
+      '/assets/tool4pokemon8th02.png'
+    ],
+    refLinks: [
+      {
+        title: '紹介記事',
+        url: 'https://trap.jp/post/880/'
+      },
+      {
+        title: 'クライアント(ソースコード)',
+        url: 'https://github.com/ryoha000/tool4pokemon8th'
       },
     ]
   },
   {
-    title: 'エロゲランチャー',
-    subtitle: 'エロゲ界のSteamを目指して',
+    title: 'booQ',
+    subtitle: '備品・書籍管理サービス',
     description: [
-      '自動でインストールしたエロゲを探索して、自分で設定することなく起動やプレイ時間の管理ができるエロゲランチャーです。',
-      '個人プロジェクトです。',
-      'デスクトップアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
-      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています',
+      'サークル内で備品・書籍を管理するニーズがあったため作成されました。',
+      'サークル内の複数人プロジェクトです。',
+      'クライアントは Vue + JavaScript で作成しています。',
+      'サーバーサイドは VPS + Go (+ echo + GORM) を使用しています',
     ],
     imgURLs: [
-      '/assets/es-client-header.png',
-      '/assets/es-client-home-activity.png'
+      '/assets/booQ01.png',
+      '/assets/booQ02.png'
     ],
     refLinks: [
       {
-        title: 'ユーザードキュメント',
-        url: 'https://ryoha000.hatenablog.com/entry/2020/08/02/215327'
+        title: '紹介記事',
+        url: 'https://trap.jp/post/643/'
       },
       {
-        title: 'Tweet',
-        url: 'https://twitter.com/ryoha_0216/status/1269247713213706242?s=20'
-      },
-      {
-        title: 'クライアント(ソースコード)',
-        url: 'https://github.com/ryoha000/es-client'
-      },
-      {
-        title: 'サーバー(ソースコード)',
-        url: 'https://github.com/ryoha000/esc_server'
+        title: 'ソースコード(クライアント、サーバー共通)',
+        url: 'https://github.com/traPtitech/booQ'
       },
     ]
   },
   {
-    title: 'エロゲランチャー',
-    subtitle: 'エロゲ界のSteamを目指して',
+    title: 'エロゲ中古価格アプリ',
+    subtitle: '中古エロゲの価格.com',
     description: [
-      '自動でインストールしたエロゲを探索して、自分で設定することなく起動やプレイ時間の管理ができるエロゲランチャーです。',
+      'エロゲの評価を見れ(批評空間様のデータをお借りしています)(https://erogamescape.dyndns.org/~ap2/ero/toukei_kaiseki/)、Sofmap, Amazon, 駿河屋での中古エロゲの通販価格を見れます。',
       '個人プロジェクトです。',
-      'デスクトップアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
-      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています',
+      '現状公開していません。',
+      '画像が荒いのは資料が残ってなくて発表の動画からキャプチャしてきたからです。',
+      'はじめてのプロダクトです。',
+      'スマホWebアプリケーションとして作っていて React Native + Expo + TypeScript(一部) で作成しています。',
+      'サーバーサイドは GCP(GCE) + Go (+ echo + sqlx) を使用しています',
     ],
     imgURLs: [
-      '/assets/es-client-header.png',
-      '/assets/es-client-home-activity.png'
+      '/assets/t01.png',
+      '/assets/t02.png',
+      '/assets/t03.png'
     ],
     refLinks: [
       {
-        title: 'ユーザードキュメント',
-        url: 'https://ryoha000.hatenablog.com/entry/2020/08/02/215327'
-      },
-      {
-        title: 'Tweet',
-        url: 'https://twitter.com/ryoha_0216/status/1269247713213706242?s=20'
-      },
-      {
         title: 'クライアント(ソースコード)',
-        url: 'https://github.com/ryoha000/es-client'
+        url: 'https://github.com/ryoha000/t-client'
       },
       {
         title: 'サーバー(ソースコード)',
-        url: 'https://github.com/ryoha000/esc_server'
-      },
-    ]
-  },
-  {
-    title: 'エロゲランチャー',
-    subtitle: 'エロゲ界のSteamを目指して',
-    description: [
-      '自動でインストールしたエロゲを探索して、自分で設定することなく起動やプレイ時間の管理ができるエロゲランチャーです。',
-      '個人プロジェクトです。',
-      'デスクトップアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
-      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています',
-    ],
-    imgURLs: [
-      '/assets/es-client-header.png',
-      '/assets/es-client-home-activity.png'
-    ],
-    refLinks: [
-      {
-        title: 'ユーザードキュメント',
-        url: 'https://ryoha000.hatenablog.com/entry/2020/08/02/215327'
-      },
-      {
-        title: 'Tweet',
-        url: 'https://twitter.com/ryoha_0216/status/1269247713213706242?s=20'
-      },
-      {
-        title: 'クライアント(ソースコード)',
-        url: 'https://github.com/ryoha000/es-client'
-      },
-      {
-        title: 'サーバー(ソースコード)',
-        url: 'https://github.com/ryoha000/esc_server'
-      },
-    ]
-  },
-  {
-    title: 'エロゲランチャー',
-    subtitle: 'エロゲ界のSteamを目指して',
-    description: [
-      '自動でインストールしたエロゲを探索して、自分で設定することなく起動やプレイ時間の管理ができるエロゲランチャーです。',
-      '個人プロジェクトです。',
-      'デスクトップアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
-      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています',
-    ],
-    imgURLs: [
-      '/assets/es-client-header.png',
-      '/assets/es-client-home-activity.png'
-    ],
-    refLinks: [
-      {
-        title: 'ユーザードキュメント',
-        url: 'https://ryoha000.hatenablog.com/entry/2020/08/02/215327'
-      },
-      {
-        title: 'Tweet',
-        url: 'https://twitter.com/ryoha_0216/status/1269247713213706242?s=20'
-      },
-      {
-        title: 'クライアント(ソースコード)',
-        url: 'https://github.com/ryoha000/es-client'
-      },
-      {
-        title: 'サーバー(ソースコード)',
-        url: 'https://github.com/ryoha000/esc_server'
-      },
-    ]
-  },
-  {
-    title: 'エロゲランチャー',
-    subtitle: 'エロゲ界のSteamを目指して',
-    description: [
-      '自動でインストールしたエロゲを探索して、自分で設定することなく起動やプレイ時間の管理ができるエロゲランチャーです。',
-      '個人プロジェクトです。',
-      'デスクトップアプリケーションとして作っていて electron + Vue2.0 + composition api + TypeScript で作成しています。',
-      'サーバーサイドは GCP(GCE) + Rust (+ Actix web + Diesel) を使用しています',
-    ],
-    imgURLs: [
-      '/assets/es-client-header.png',
-      '/assets/es-client-home-activity.png'
-    ],
-    refLinks: [
-      {
-        title: 'ユーザードキュメント',
-        url: 'https://ryoha000.hatenablog.com/entry/2020/08/02/215327'
-      },
-      {
-        title: 'Tweet',
-        url: 'https://twitter.com/ryoha_0216/status/1269247713213706242?s=20'
-      },
-      {
-        title: 'クライアント(ソースコード)',
-        url: 'https://github.com/ryoha000/es-client'
-      },
-      {
-        title: 'サーバー(ソースコード)',
-        url: 'https://github.com/ryoha000/esc_server'
+        url: 'https://github.com/ryoha000/t-cloud'
       },
     ]
   }
