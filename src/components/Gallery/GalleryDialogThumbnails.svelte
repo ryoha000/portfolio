ca8260d358c868367b73ff2e1c4386b4a2ad4345
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-09-06 07:45:35 +0900

    add GalleryDialogThumbnails Animation

--- src/components/Gallery/GalleryDialogThumbnails.svelte
+++ src/components/Gallery/GalleryDialogThumbnails.svelte
@@ -25,17 +25,30 @@
   }
   .thumb {
     height: 100%;
+    width: 30vw;
+    max-width: 192px;
     cursor: pointer;
+    display: flex;
+    justify-content: center;
   }
   img {
     height: 100%;
+  }
+  .selected {
+    position: absolute;
+    transition: transform 1s;
+    height: 100%;
+    width: 30vw;
+    max-width: 192px;
+    border: 2px red solid;
   }
 </style>
 
 <div class="container">
   {#each imgURLs as src, i}
     <div class="thumb" on:click="{() => select(i)}">
-      <img {src} alt="{`${i}`}" style="{imgIndex === i ? 'border: 2px red solid;' : ''}" />
+      <img {src} alt="{`${i}`}" />
     </div>
   {/each}
+  <div class="selected" style="{`transform: translateX(min(${30 * imgIndex}vw, ${192 * imgIndex}px));`}"></div>
 </div>
