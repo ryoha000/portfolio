f926460e4bd80a285b003078b22fb1689ceed878
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-09-08 08:36:58 +0900

    add SelifDialog

--- src/components/Charactors/CharactorVisual.svelte
+++ src/components/Charactors/CharactorVisual.svelte
@@ -2,8 +2,37 @@
   import type { CharactorData } from './charactorData'
   import CharactorImage from './CharactorImage.svelte'
   import Hukidashi from './Hukidashi.svelte'
+  import { speak } from '../../store/index'
+  import { fade } from 'svelte/transition';
 
   export let data: CharactorData
+
+  let text = ''
+  let allText = ''
+  let timer = 0
+
+  const unsubscribe = speak.subscribe(s => {
+    if (!s.speaking) {
+      clearInterval(timer)
+      text = ''
+      allText = ''
+    }
+		if (s.speaking && allText !== s.selif) {
+      text = ''
+      allText = s.selif
+      typing(s.selif)
+    }
+  });
+  
+  const typing = (selif: string) => {
+    if (text.length === selif.length) {
+      return
+    }
+    text = selif.slice(0, text.length + 1)
+    timer = setTimeout(() => {
+      typing(selif)
+    }, 50)
+  }
 </script>
 
 <style>
@@ -11,6 +40,18 @@
     margin: auto;
     min-width: 30%;
     position: relative;
+  }
+  .selif {
+    position: absolute;
+    top: 35%;
+    height: 30%;
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0.8);
+    border: solid rgba(0, 0, 0, 0.3) 1px;
+    word-break : break-all;
+    padding: 8px;
+    border-radius: 8px;
+    transform: rotate(0.03deg);
   }
   .hukidashi {
     position: absolute;
@@ -32,10 +73,16 @@
 
 <div class="container">
   <CharactorImage {data} />
-  <div class="hukidashi leftHukidashi">
-    <Hukidashi label="ユーザー" url="{data.account}" />
-  </div>
-  <div class="hukidashi rightHukidashi">
-    <Hukidashi label="元ネタ" url="{data.url}" />
-  </div>
+  {#if $speak.speaking && data.voices.some(v => v.text === $speak.selif)}
+    <div class="selif" transition:fade>
+      {text}
+    </div>
+  {:else}
+    <div class="hukidashi leftHukidashi">
+      <Hukidashi label="ユーザー" url="{data.account}" />
+    </div>
+    <div class="hukidashi rightHukidashi">
+      <Hukidashi label="元ネタ" url="{data.url}" />
+    </div>
+  {/if}
 </div>
